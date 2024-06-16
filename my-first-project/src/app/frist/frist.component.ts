03bd4374c86d39a8979b7a2328cc7eb846ee0f74
MattSouza14 <mateussouzabr1@gmail.com>
2024-06-16 11:04:33 -0300

    Adição do primeiro projeto teste em Angular

---
+++ my-first-project/src/app/frist/frist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frist',
+  standalone: true,
+  imports: [],
+  templateUrl: './frist.component.html',
+  styleUrl: './frist.component.css'
+})
+export class FristComponent {
+
+}
